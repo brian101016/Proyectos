7e7e85a4aa52cf158bfc3e2d08224f9408a570ab
Brian Woolfolk <brianwoolfolk@hotmail.com>
2024-03-13 11:21:59 -0700

    Puzzle big refactor

--- WEB/Puzzle/puzzle.js
+++ WEB/Puzzle/puzzle.js
@@ -10,18 +10,29 @@ let playing = false;
 let auto = false;
 let generating = false;
 let consecutive_num = null;
-let chartSize = 4;
 let url = "";
 
 // ---------------------------------------------------------------------- STRUCTURES
-const prevCoords = { x: 0, y: 0 };
+const chartSize = { x: 4, y: 4 };
+const emptyCoords = { x: 0, y: 0 };
 const exterior = { x: 0, y: 0 };
 /** @type { (HTMLDivElement | null)[] } */
 const pieces = [];
 /** @type {{ x: number, y: number }[]} */
 const coords = [];
-/** @type {{ x: number | null, y: number | null }} */
-const touchCoords = { x: null, y: null };
+/**
+ * Guarda el index según la posición en el chart, de los elementos de coords y pieces.
+ * También guarda la pieza que está en la posición del exterior al final del arreglo.
+ * Regresa -1 en caso de que haya un lugar vacío.
+ * @type { number[] }
+ * @example pieces[positions[0]]; // Regresa la pieza que está en la posición 0 del chart
+ * pieces[positions[pieces.length]]; // Regresa la pieza que está en el exterior
+ * coords[positions[4]] === parseCoords(4);
+ * pieces[positions[parseIndex({x: 0, y: 0})]]; // Buscar por coordenadas
+ * positions[parseIndex(emptyCoords)] === -1; // Siempre
+ */
+const positions = [];
+const touchCoords = { x: 0, y: 0 };
 
 // ---------------------------------------------------------------------- CONSTANTS
 const cont = document.getElementById("chart");
@@ -84,16 +95,15 @@ function handlePreviewToggle() {
  * @param {*} e El evento del teclado
  */
 function handleKeyDown(e) {
-  const value = invert.checked ? -100 : 100;
-  let ntt = prevCoords.y;
-  let nll = prevCoords.x;
-  if (e.code === "ArrowUp" || e.code === "KeyW") ntt += value;
-  else if (e.code === "ArrowDown" || e.code === "KeyS") ntt -= value;
-  else if (e.code === "ArrowRight" || e.code === "KeyD") nll -= value;
-  else if (e.code === "ArrowLeft" || e.code === "KeyA") nll += value;
+  let direction = "up";
+
+  if (e.code === "ArrowUp" || e.code === "KeyW") direction = "up";
+  else if (e.code === "ArrowDown" || e.code === "KeyS") direction = "down";
+  else if (e.code === "ArrowRight" || e.code === "KeyD") direction = "right";
+  else if (e.code === "ArrowLeft" || e.code === "KeyA") direction = "left";
   else return;
 
-  movePiece(ntt, nll);
+  movePiece(direction);
 }
 
 // -------------------------------------------------- HANDLE TOUCH START
@@ -124,19 +134,14 @@ function handleTouchMove(evt) {
   const xDiff = touchCoords.x - xUp;
   const yDiff = touchCoords.y - yUp;
 
-  const value = invert.checked ? 100 : -100;
-  let ntt = prevCoords.y;
-  let nll = prevCoords.x;
+  let direction = "up";
 
   if (Math.abs(xDiff) > Math.abs(yDiff)) {
-    if (xDiff > 0) nll -= value;
-    else nll += value;
-  } else {
-    if (yDiff > 0) ntt -= value;
-    else ntt += value;
-  }
+    if (xDiff > 0) direction = "right";
+    else direction = "left";
+  } else if (yDiff > 0) direction = "down";
 
-  movePiece(ntt, nll);
+  movePiece(direction);
   touchCoords.y = touchCoords.x = null;
 }
 // #endregion
@@ -168,39 +173,6 @@ function setAuto(activate) {
   if (auto) console.log("START AUTO");
   else console.log("END AUTO");
 }
-
-// ---------------------------------------------------------------------- FIND NEXT WRONG
-/**
- * Función principal para conocer cuál pieza está mal colocada, y entonces comenzar el auto.
- */
-function findNextWrong() {
-  console.log("Finding...");
-
-  let index = 0;
-  for (const piece of pieces) {
-    const im_in = {
-      x: parseInt(piece.style.left),
-      y: parseInt(piece.style.top),
-    };
-    const need_to_be = {
-      x: parseInt(piece.style.backgroundPositionX),
-      y: parseInt(piece.style.backgroundPositionY),
-    };
-
-    index++;
-
-    let wrong = "";
-    if (im_in.x + need_to_be.x !== 0) wrong += "   Wrong X coord\n";
-    if (im_in.y + need_to_be.y !== 0) wrong += "   Wrong Y coord";
-    if (wrong) {
-      console.log(index + ".- ", im_in, need_to_be);
-      console.log(wrong);
-      break;
-    }
-  }
-
-  console.log("END FOUND");
-}
 // #endregion
 
 // #region ##################################################################################### STATUS FUNCTIONS
@@ -209,10 +181,12 @@ function findNextWrong() {
  * Reinicia todas las variables a su valor por defecto, se usa dentro de `block`.
  */
 function reset() {
-  chartSize = inp.value ? parseInt(inp.value) : chartSize;
-  cont.style.height = cont.style.width = chartSize * 100 + "px";
-  prevCoords.x = exterior.x = chartSize * 100;
-  prevCoords.y = exterior.y = (chartSize - 1) * 100;
+  chartSize.x = parseInt(inp.value) || 4;
+  chartSize.y = parseInt(inp.value) || 4;
+  cont.style.width = chartSize.x * 100 + "px";
+  cont.style.height = chartSize.y * 100 + "px";
+  emptyCoords.x = exterior.x = chartSize.x;
+  emptyCoords.y = exterior.y = chartSize.y - 1;
   playing = auto = false;
   consecutive_num = null;
 }
@@ -247,7 +221,30 @@ function block(disabled) {
  * @returns {{ x: number, y: number }} Pair of coords
  */
 function parseCoords(index) {
-  return { x: index % chartSize, y: Math.floor(index / chartSize) };
+  return { x: index % chartSize.x, y: Math.floor(index / chartSize.x) };
+}
+
+// ---------------------------------------------------------------------- PARSE INDEX
+/**
+ * Convierte unas coordenadas en un índice para buscar dentro del arreglo pieces.
+ * @param {{x: number, y: number}} cor Coordenadas
+ * @returns {number} regresa un número con el posible índice
+ */
+function parseIndex(cor) {
+  return cor.y * chartSize.x + cor.x;
+}
+
+// ---------------------------------------------------------------------- CHECK VALIDITY
+/**
+ * Revisa si un par de coordenadas son válidas
+ * @param {number} indexx la coordenada x o el índice
+ * @param {number | undefined} yy la coordenada y, si no existe, entonces se considera al indexx como índice
+ * @returns {boolean} dependiendo de si las coords o index son válidas o no
+ */
+function checkValidity(indexx, yy) {
+  if (indexx < 0 || yy < 0) return false;
+  if (yy === undefined) return indexx < pieces.length;
+  return 0 <= yy && yy < chartSize.y && 0 <= indexx && indexx < chartSize.x;
 }
 
 // ---------------------------------------------------------------------- GEN START
@@ -258,31 +255,50 @@ function parseCoords(index) {
 async function genStart() {
   if (generating) return;
   block((generating = true));
+
   url = "";
+  const xx = chartSize.x * 100;
+  const yy = chartSize.y * 100;
+
   if (photo.files.length) {
     url = URL.createObjectURL(photo.files[0]);
     setTimeout(() => URL.revokeObjectURL(url), 10000);
   } else {
-    const res = await fetch("https://picsum.photos/" + chartSize * 100);
+    const res = await fetch(`https://picsum.photos/${xx}/${yy}`).catch(
+      (e) => null
+    );
+    if (!res) {
+      block((generating = false));
+      playing = false;
+      stat.textContent = "Failed to load!";
+      return;
+    }
     url = res.url;
   }
 
   preview.style.backgroundImage = `url(${url})`;
-  preview.style.width = preview.style.backgroundSize = chartSize * 100 + "px";
+  preview.style.width = xx + "px";
+  preview.style.height = yy + "px";
+  preview.style.backgroundSize = `${xx}px ${yy}px`;
   cont.replaceChildren();
-  pieces.length = coords.length = 0;
-  pieces.length = coords.length = chartSize * chartSize;
+  const max = chartSize.x * chartSize.y;
+  pieces.length = coords.length = positions.length = 0;
+  pieces.length = coords.length = max;
+  positions.length = max + 1;
 
   // ESTE CICLO ES ESTÉTICO PARA QUE SE VEAN LAS PIEZAS ANTES DE REVOLVERLAS
-  for (let i = 0; i < coords.length; i++) updatePiece(i);
-  await stall(500); // MEDIO SEGUNDO PARA VER piece PUZZLE COMPLETO
+  for (let i = 0; i < max; i++) updatePiece(i);
+  await stall(500); // MEDIO SEGUNDO PARA VER EL PUZZLE COMPLETO
 
-  for (let i = 0; i < coords.length - 1; i++) {
-    shufflePieces(i, coords.length - 2);
+  for (let i = 0; i < max - 1; i++) {
+    shufflePieces(i, max - 2);
     await stall(150);
   }
 
-  // updatePiece(i); // ESTE NO ES NECESARIO SI SE ACTIVA piece CICLO ESTÉTICO
+  // updatePiece(i); // ESTE NO ES NECESARIO SI SE ACTIVA EL CICLO ESTÉTICO
+
+  positions[max - 1] = max - 1; // Agregar la última pieza bien colocada
+  positions[max] = -1; // Agregar el espacio vacío exterior
 
   block((generating = false));
 }
@@ -319,6 +335,10 @@ function shufflePieces(curri, max) {
   coords[n1] = currentCoords;
   coords[n2] = otherCoords1;
 
+  positions[parseIndex(otherCoords2)] = curri;
+  positions[parseIndex(currentCoords)] = n1;
+  positions[parseIndex(otherCoords1)] = n2;
+
   updatePiece(curri);
   updatePiece(n1);
   updatePiece(n2);
@@ -333,17 +353,17 @@ function shufflePieces(curri, max) {
  */
 function updatePiece(index) {
   const piece = pieces[index] || document.createElement("div");
-  const actualCoords = parseCoords(index);
-  const coord = coords[index] || actualCoords;
+  const chartCoords = parseCoords(index);
+  const coord = coords[index] || chartCoords;
 
   piece.src = url;
   piece.className = "piece";
   piece.style.top = coord.y * 100 + "px";
   piece.style.left = coord.x * 100 + "px";
   piece.style.backgroundImage = `url(${url})`;
-  piece.style.backgroundPositionX = `-${actualCoords.x * 100}px`;
-  piece.style.backgroundPositionY = `-${actualCoords.y * 100}px`;
-  piece.style.backgroundSize = chartSize * 100 + "px";
+  piece.style.backgroundPositionX = `-${chartCoords.x * 100}px`;
+  piece.style.backgroundPositionY = `-${chartCoords.y * 100}px`;
+  piece.style.backgroundSize = `${chartSize.x * 100}px ${chartSize.y * 100}px`;
 
   if (!pieces[index]) {
     pieces[index] = piece;
@@ -353,37 +373,61 @@ function updatePiece(index) {
 }
 
 // ---------------------------------------------------------------------- MOVE PIECE
-// REFACTOR
-function movePiece(ntt, nll) {
+/**
+ * Mover una pieza con respecto al espacio vacío
+ * @param {"up" | "down" | "left" | "right"} direction Dirección a la que moverse
+ */
+function movePiece(direction) {
   if (auto || !playing) return console.log("CANT MOVE IN AUTO");
 
+  const value = invert.checked ? -1 : 1;
+  let { x, y } = emptyCoords;
+
+  if (direction === "up") y += value;
+  else if (direction === "down") y -= value;
+  else if (direction === "right") x -= value;
+  else x += value;
+
   let check = false;
-  if (0 <= ntt && ntt < chartSize * 100 && 0 <= nll && nll < chartSize * 100) {
-  } else if (ntt === exterior.y && nll === exterior.x) check = true;
+  if (checkValidity(x, y)) check = false;
+  else if (y === exterior.y && x === exterior.x) check = true;
   else return;
-  let win = check;
 
-  for (const piece of pieces) {
-    let tt = parseInt(piece.style.top);
-    let ll = parseInt(piece.style.left);
+  const magicIndex = positions[parseIndex({ x, y })];
+  const piece = pieces[magicIndex];
+  if (!piece) return;
 
-    if (tt === ntt && ll === nll) {
-      piece.style.left = prevCoords.x + "px";
-      piece.style.top = prevCoords.y + "px";
-      prevCoords.x = ll;
-      prevCoords.y = tt;
+  // MOVER PIEZA FÍSICAMENTE
+  piece.style.left = emptyCoords.x * 100 + "px";
+  piece.style.top = emptyCoords.y * 100 + "px";
 
-      if (!check) break;
-    }
+  // ACTUALIZAR LAS COORDS
+  coords[magicIndex].x = emptyCoords.x;
+  coords[magicIndex].y = emptyCoords.y;
 
-    win =
-      win &&
-      parseInt(piece.style.top) + parseInt(piece.style.backgroundPositionY) ===
-        0 &&
-      parseInt(piece.style.left) + parseInt(piece.style.backgroundPositionX) ===
-        0;
-  }
+  // ACTUALIZAR LAS POSICIONES
+  positions[parseIndex(emptyCoords)] = magicIndex;
+  positions[parseIndex({ x, y })] = -1;
 
-  if (win) won();
+  // MOVER EL ESPACIO VACÍO
+  emptyCoords.x = x;
+  emptyCoords.y = y;
+
+  if (check) findNextWrong();
+}
+
+// ---------------------------------------------------------------------- FIND NEXT WRONG
+/**
+ * Revisa a ver si la pieza está en su lugar, si no se indica index entonces regresa el
+ * primer index que esté incorrecto, si todo está bien marca -1
+ * @param {number | undefined} index Un index a verificar, o undefined para todos
+ */
+function findNextWrong(index) {
+  if (index !== undefined) return positions[index] === index;
+
+  const wrongPlace = positions.findIndex((v, i) => v !== i);
+  if (wrongPlace === pieces.length) won();
+
+  return wrongPlace;
 }
 // #endregion
